0d5700867d7ed0adeff9cf46547c143070117b6e
tom <tomspencerlondon@gmail.com>
2023-05-16 21:31:31 +0100

    add my-app-2 for use with the SayHelloRecipe

---
+++ src/main/java/com/mycompany/app2/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public String hello() {
+        return "Hello there from com.mycompany.app2.App!!!!!";
+    }
+}
